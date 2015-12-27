9de3a9a96a3c206938058f99981d286d30db972f
maiordom <maiordom@yandex.ru>
2015-12-27 14:58:28 +0300

    update

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ var logger = function() {
 
 exec(util.format('cp %s/src/.editorconfig %s', moduleDir, execDir), logger);
 exec(util.format('cp %s/src/.eslintrc.js %s', moduleDir, execDir), logger);
-exec(util.format('cp %s/src/gulpfile.js %s', moduleDir, execDir), logger);
 exec(util.format('cp %s/src/pre-commit.js %s', moduleDir, execDir), logger);
-exec(util.format('cp %s/demo %s', moduleDir, execDir), logger);
-exec(util.format('cp %s/test %s', moduleDir, execDir), logger);
+
+exec(util.format('cp %s/src/gulpfile.js %s', moduleDir, execDir), logger);
+exec(util.format('cp %s/src/demo %s', moduleDir, execDir), logger);
+exec(util.format('cp %s/src/test %s', moduleDir, execDir), logger);
 exec(util.format('mkdir %s/src', execDir), logger);
 exec(util.format('mkdir %s/dist', execDir), logger);
