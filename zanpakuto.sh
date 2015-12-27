f59167c8afad008781a6be41f442dff4410cdfbf
maiordom <maiordom@yandex.ru>
2015-12-27 20:59:35 +0300

    update

--- zanpakuto.sh
+++ zanpakuto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 EXEC_DIR=$(pwd)
-MODULE_DIR=eval cd $(dirname $0) && pwd
+MODULE_DIR=$(cd $(dirname $0) && cd $(dirname $(readlink $0)) && pwd)
 
 cp -vi $MODULE_DIR/src/.editorconfig $EXEC_DIR
 cp -vi $MODULE_DIR/src/.eslintrc.js $EXEC_DIR
 cp -vi $MODULE_DIR/src/pre-commit.js $EXEC_DIR
-cp -vi $MODULE_DIR/src/gulpfuke.js $EXEC_DIR
+cp -vi $MODULE_DIR/src/gulpfile.js $EXEC_DIR
 cp -vir $MODULE_DIR/src/demo $EXEC_DIR
 cp -vir $MODULE_DIR/src/test $EXEC_DIR
 mkdir -v $EXEC_DIR/src
