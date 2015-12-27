4fba89c3f799beedb6eee473cdbce3bf20eeb858
maiordom <maiordom@yandex.ru>
2015-12-27 14:32:25 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('Hi, Im Zanpakuto!');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var util = require('util');
+
+var execDir = process.cwd();
+var moduleDir = __dirname;
+var logger = function() {
+    console.log(arguments);
+};
+
+exec(util.format('cp %s/src/.eslintrc.js %s', moduleDir, execDir), logger);
+exec(util.format('cp %s/src/gulpfile.js %s', moduleDir, execDir), logger);
+exec(util.format('cp %s/src/pre-commit.js %s', moduleDir, execDir), logger);
+exec(util.format('cp %s/demo %s', moduleDir, execDir), logger);
+exec(util.format('cp %s/test %s', moduleDir, execDir), logger);
+exec(util.format('mkdir %s/src', execDir), logger);
+exec(util.format('mkdir %s/dist', execDir), logger);
